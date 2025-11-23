71f1e6716819c369f4ee63e6b4336a51e1b26824
SamarthVats <143289583+samarthvats04@users.noreply.github.com>
2025-11-23 21:06:39 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,8 +27,8 @@ export default function RootLayout({ children }) {
       <body className="bg-black">
         {!hideHeader && <Header />}
         {children}
+        <Analytics />
       </body>
-      <Analytics />
     </html>
     
   );
