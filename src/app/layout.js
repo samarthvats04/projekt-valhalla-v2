e477ba8c7f26c7078ae040d9a33c89b8ff19698c
SamarthVats <143289583+samarthvats04@users.noreply.github.com>
2025-12-15 00:55:47 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,41 @@ export default function RootLayout({ children }) {
       className={metamorphous.variable}
       suppressHydrationWarning={true}
     >
+      <head>
+        <style jsx global>{`
+          /* Custom Scrollbar */
+          ::-webkit-scrollbar {
+            width: 10px;
+          }
+          
+          ::-webkit-scrollbar-track {
+            background: transparent;
+          }
+
+          ::-webkit-scrollbar-track-piece {
+            background: transparent;
+          }
+
+          ::-webkit-scrollbar-thumb {
+            background: #3f3f46;
+            border-radius: 5px;
+          }
+          
+          ::-webkit-scrollbar-thumb:hover {
+            background: #71717a;
+          }
+          ::-webkit-scrollbar-button {
+            background: transparent;
+            width: 0;
+            height: 0;
+          }
+          /* Firefox */
+          * {
+            scrollbar-width: thin;
+            scrollbar-color: #3f3f46 #0a0a0a;
+          }
+        `}</style>
+      </head>
       <body className="bg-black">
         {!hideHeader && <Header />}
         {children}
